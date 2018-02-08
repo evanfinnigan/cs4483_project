d664cfc03caf574997d0a0a7134b233de2cbdcbe
Evan David McClenaghan Finnigan <efinnig@uwo.ca>
2018-02-07 19:57:42 -0500

    Initial project base commit. Haven't made a gitignore yet, so there may be some unnecessary files

---
+++ Vikings/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    int width = 4;
+    int height = 4;
+
+	void FixedUpdate () {
+
+		if (transform.position.x - target.position.x < -1*width)
+        {
+            StartCoroutine(MoveCameraRightCo());
+        }
+
+        if (transform.position.x - target.position.x > width)
+        {
+            StartCoroutine(MoveCameraLeftCo());
+        }
+
+        if (transform.position.y - target.position.y < -1*height)
+        {
+            StartCoroutine(MoveCameraDownCo());
+        }
+
+        if (transform.position.y - target.position.y > height)
+        {
+            StartCoroutine(MoveCameraUpCo());
+        }
+
+    }
+
+    private IEnumerator MoveCameraRightCo()
+    {
+        while (transform.position.x - target.position.x < width)
+        {
+            transform.position += new Vector3(0.125f, 0f);
+            yield return null;
+        }
+    }
+
+    private IEnumerator MoveCameraLeftCo()
+    {
+        while (transform.position.x - target.position.x > -1*width)
+        {
+            transform.position -= new Vector3(0.125f, 0f);
+            yield return null;
+        }
+    }
+
+    private IEnumerator MoveCameraDownCo()
+    {
+        while (transform.position.y - target.position.y < height)
+        {
+            transform.position += new Vector3(0f, 0.125f);
+            yield return null;
+        }
+    }
+
+    private IEnumerator MoveCameraUpCo()
+    {
+        while (transform.position.y - target.position.y > -1*height)
+        {
+            transform.position -= new Vector3(0f, 0.125f);
+            yield return null;
+        }
+    }
+}
---
+++ Vikings/Assets/Scripts/MoveBoat.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBoat : MonoBehaviour {
+
+    int xCoordinate = 0;
+    int yCoordinate = 0;
+
+    bool move = false;
+    Vector3 movement;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            xCoordinate++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            xCoordinate--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            yCoordinate++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            yCoordinate--;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        movement = Vector3.zero;
+
+        if (transform.position.x < xCoordinate)
+        {
+            movement += new Vector3(0.125f, 0f, 0f);
+        }
+
+        if (transform.position.x > xCoordinate)
+        {
+            movement += new Vector3(-0.125f, 0f, 0f);
+        }
+
+        if (transform.position.y < yCoordinate)
+        {
+            movement += new Vector3(0f, 0.125f, 0f);
+        }
+
+        if (transform.position.y > yCoordinate)
+        {
+            movement += new Vector3(0f, -0.125f, 0f);
+        }
+
+        transform.position += movement;
+    }    
+}
