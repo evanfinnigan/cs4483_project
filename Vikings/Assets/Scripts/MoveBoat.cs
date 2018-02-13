f49c6fb17e8a585f25b83d479cf867beb0988edd
Evan David McClenaghan Finnigan <efinnig@uwo.ca>
2018-02-13 18:15:44 -0500

    adding collision map, and displaying target position. the actual boat can go over land but the target can't. not using unity 2d collisions, just doing it manually based on a check as to whether there is a tile on the collision map at the desired position

--- Vikings/Assets/Scripts/MoveBoat.cs
+++ Vikings/Assets/Scripts/MoveBoat.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MoveBoat : MonoBehaviour {
+
+    public GameObject targetUISprite;
+
+    public Tilemap collisionMap;
 
     int xCoordinate = 0;
     int yCoordinate = 0;
@@ -10,32 +15,39 @@ public class MoveBoat : MonoBehaviour {
     bool move = false;
     Vector3 movement;
 
+    Vector3Int uiPosition;
+
     private void Start()
     {
-        
+        uiPosition = new Vector3Int(0, 0, 0);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) && !collisionMap.HasTile(uiPosition + (new Vector3Int(2,0,0))))
         {
             xCoordinate++;
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && !collisionMap.HasTile(uiPosition + (new Vector3Int(-2, 0, 0))))
         {
             xCoordinate--;
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !collisionMap.HasTile(uiPosition + (new Vector3Int(0, 2, 0))))
         {
             yCoordinate++;
         }
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow) && !collisionMap.HasTile(uiPosition + (new Vector3Int(0, -2, 0))))
         {
             yCoordinate--;
         }
+
+        uiPosition = new Vector3Int(xCoordinate*2, yCoordinate*2, 0);
+
+        if (!collisionMap.HasTile(uiPosition))
+            targetUISprite.transform.position = new Vector2(xCoordinate,yCoordinate);
     }
 
     private void FixedUpdate()
