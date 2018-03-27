cb6984f1210aac9c6c5ef1b8180feb0024d0ae6b
Tim Etchells <tetchel@gmail.com>
2018-03-27 18:52:14 -0400

    Add end of level, but it doesn't do anything yet

---
+++ Vikings/Assets/Scripts/land/Exit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        GameObject other = collision.gameObject;
+        if(other.GetComponent<PlayerController>() != null) {
+            if(FindObjectsOfType<EnemyController>().Length > 0) {
+                Debug.Log("Enemies remain");
+            }
+            else {
+                Debug.Log("Good job ur done the level");
+            }
+        }
+    }
+}
